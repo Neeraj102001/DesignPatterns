5208b31c588165ea067df7e079d95fc804073127
neerajyadav01 <neeraj.yadav01@nagarro.com>
2023-09-18 01:03:12 +0530

    added dotnet project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static NotificationApp.FacadeDesign;
+using static NotificationApp.ObserverDesign;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the notification sending mode (email or sms):");
+        string mode = Console.ReadLine();//reading notification sending mode  
+
+        Console.WriteLine("Enter subject:");
+        string subject = Console.ReadLine();
+
+        Console.WriteLine("Enter body:");
+        string body = Console.ReadLine();
+
+       
+        var notificationFacade = new NotificationFacade(mode);  // Instantiation of the necessary objects of the Facade Design Pattern
+        notificationFacade.ActualSendNotification(subject, body);  //printing the sended message on screen using the selected mode and to All Users 
+
+        
+        
+
+        // Instantiation and notifying subscribed observers(or Users)
+        var logger = new NotifyingMultipleUsers();
+        logger.NotificationtoAllSubscribedUsers("Notification sent successfully to the subscribed users!!!");
+        
+        Console.ReadKey();
+    }
+}
